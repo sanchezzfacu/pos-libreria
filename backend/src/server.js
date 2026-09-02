6fb248077c72cac3c5008a2ef276a0070cd25fdc
clasificadosargentina <sanchezfacundocristian@hotmail.com>
2026-09-02 11:00:52 -0300

    asd

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ app.use(morgan("dev"));
 
 app.use(cors({
   origin: process.env.FRONTEND_URL || "*",
-  // credentials: true,
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
